a1aafa373883ebd49eb270ab85e103a3d12be41c
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-04-11 13:12:50 +0100

    Implement hide/show image

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -9,6 +9,8 @@ export class ProductListComponent {
   pageTitle : string = 'Product List [v2]' ;
   imageWidth : number = 50;
   imageMargin: number = 2;
+  showImage : boolean = false ;
+
   products: any[] = [
     {
       "productId": 2,
@@ -31,4 +33,8 @@ export class ProductListComponent {
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
   }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
