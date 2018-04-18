3ec481f83dede5f3b85998faebd9a644be01d8cd
Dr. Lionel Saliou <lionel.saliou@ge.com>
2018-04-18 13:11:42 +0100

    Implement route in code and guards (module_12)
    This project now uses a TSLINTER that forces to use `const`
    instead of `let` should the variable only be used once.
    
    Likewise, we use the "+" shorthand to convert a string to a number
    implicitely.
    
    TODO:
      1) Finish the detail product component view to display the
         product information
      2) Improve the product service so that it can gather the
         specific data of a given Product ID
    
         ---- Lionel Saliou, Ph.D`

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { IProduct } from './product';
 
 @Component({
@@ -9,9 +11,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router ) { }
 
   ngOnInit() {
+    // "+" is a Javacscript shorthand to conver a string to a number
+    // tslinter suggests using const instead of let because we do not re-assign the variable
+    const id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Hammer',
+      'productCode': 'TBX-0048',
+      'releaseDate': 'May 21, 2016',
+      'description': 'Curved claw steel hammer',
+      'price': 8.9,
+      'starRating': 4.8,
+      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
+    };
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
